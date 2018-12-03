ca434aaa221be1ac6cdd6dc6e6d48949fe505ec6
Rob Underwood <runderwood5@gmail.com>
2018-12-03 12:58:46 -0500

    Examples of how to do common FINOS object definitions

--- src/examples/Contact.ts
+++ src/examples/Contact.ts
@@ -1,8 +1,12 @@
+import "../Context.js";
+import "../FINOS/Contact.ts";
+
 interface Contact extends Context {
-    type: 'fdc3.contact',
-    id: {
-        email: string;
-        twitter?: string;
-        phone?: string;
-    }
+  type: 'fdc3.contact',
+  id: {
+    //the following comes from the common FINOS definition
+    ...contact,
+    //an optional field that FDC3 uses to extend common def
+      city?: string
+  }
 }
---
+++ src/finos/Contact.ts
@@ -0,0 +1,8 @@
+interface Contact  {
+  contact: {
+    //this should come from the common definition
+      email: string;
+      twitter?: string;
+      phone?: string;
+  }
+}
